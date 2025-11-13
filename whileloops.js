96c9f3913be823b9068e8297c53e4379505c80a2
gumfoot <nestagithaka@gmail.com>
2025-11-13 15:28:33 +0300

    Added more details to while loops

--- whileloops.js
+++ whileloops.js
@@ -5,24 +5,24 @@ while (condition){
 */
 
 
-// let sum = 0; 
-// let i = 1;
-// while(i <= 10){
-//     sum += i;
-//     console.log(sum);
-//     i++;
-// }
-
-// const num = 4;
-// let j = 0;
-// while (j <= 10){
-//     let product = num * j;
-//     console.log(`${num} x ${j} = ${product}`);
-//     j++;
-// }
-
+ let sum = 0; 
+ let i = 1;
+ while(i <= 10){
+     sum += i;
+     console.log(sum);
+     i++;
+ }
 
 const num = 4;
+let j = 0;
+while (j <= 10){
+    let product = num * j;
+    console.log(`${num} x ${j} = ${product}`);
+    j++;
+}
+
+
+const numb = 4;
 let k = 1;
 while(k <= 10){
     if(k % 2 == 0){
@@ -33,3 +33,19 @@ while(k <= 10){
     k++;
 
 }
+
+
+/*
+do...while loops
+do {
+    body;
+}
+    while(condition)
+*/
+
+let m = 10;
+do {
+    console.log(i);
+    m --;
+}
+while (m >= 0);
