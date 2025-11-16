ab1a377cedc410a89226291e214b02d50f86f170
gumfoot <nestagithaka@gmail.com>
2025-11-16 17:37:27 +0300

    Learnt functions, parameters and arguments.

--- functions.js
+++ functions.js
@@ -1,6 +1,39 @@
-//function that takes two numbers and returns their sum
-function sum(num1, num2){
-    return num1 + num2;
+//Function - Independent block of code that performs a specific task. 
+//function expression - a way to store functions in variables.
+
+
+//function functionName(function, parameter(s)){
+//       body of code;
+//}
+// functionName(argument);
+
+
+
+//function
+function greetUser(name){ //name is a parameter. - argument placeholder
+    console.log(`Hello, ${name}!`);
 }
 
-let answer = sum(2, 3);
+greetUser("Nesta");//"Nesta" is an argument.
+greetUser("Moses");
+greetUser("Nick"); //Passing different arguments to function.
+greetUser("Ian");
+
+
+//function expression
+let getPI = function(){
+    console.log("PI is 3.142!");
+}
+
+getPI();
+
+function addNumbers(num1, num2){ //multiple arguments one function.
+
+}
+
+function findSquare(num){
+    console.log("Executing...");
+    return num * num; //return statement - terminates the function.
+}
+let square = findSquare(16); //store value in square variable.
+console.log(`Square: ${square}`);
