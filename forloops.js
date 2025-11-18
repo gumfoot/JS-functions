dd2b980611b161d19dd1ec72bdf2f8c0c6ac6ca1
gumfoot <nestagithaka@gmail.com>
2025-11-18 15:12:24 +0300

    Modified forloops file

--- forloops.js
+++ forloops.js
@@ -12,3 +12,14 @@ for (let i = 1; i <= 10; i++){
 }
 console.log(sum);
 
+//for...in loops - iterates over keys of an object
+
+const cars = {
+        make : "Toyota",
+        model : "Corolla",
+        yom : 2017,
+}
+
+for (let key in cars){
+    console.log(cars[key]);
+}
