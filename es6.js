7d7dab7c751686b664ee06d750e7a539ef6c68cb
gumfoot <nestagithaka@gmail.com>
2025-11-19 15:11:42 +0300

    Starting ES6

---
+++ es6.js
@@ -0,0 +1,19 @@
+/* 
+JavaScript ES6.
+Standard specification of JS. Introduced in 2015.
+
+            
+
+        Commonly used features:
+Declarations - let and const.
+Template literals
+Default parameter values
+Arrow functions
+Classes
+Destructuring
+Modules
+Promise
+Rest Parameter
+Spread operator
+*/
+
